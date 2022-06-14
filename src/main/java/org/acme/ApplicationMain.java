fc5cc718ea5a997a665efbcb46cb87637f2e3621
Oscar Albrecht <3311764+oscarfh@users.noreply.github.com>
2022-06-14 18:55:36 -0300

    Final version of the reproducer

--- src/main/java/org/acme/ApplicationMain.java
+++ src/main/java/org/acme/ApplicationMain.java
@@ -1,62 +1,62 @@
-package org.acme;
-
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.quarkus.runtime.QuarkusApplication;
-import io.quarkus.runtime.annotations.QuarkusMain;
-import javax.enterprise.inject.spi.BeanManager;
-import javax.inject.Inject;
-import org.acme.rest.client.Extension;
-import org.acme.rest.client.ExtensionsService;
-import org.eclipse.microprofile.config.Config;
-import org.eclipse.microprofile.config.ConfigProvider;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-@QuarkusMain
-public class ApplicationMain implements QuarkusApplication {
-	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationMain.class);
-
-	@RestClient
-	ExtensionsService extensionsService;
-
-
-	@Override
-	public int run(String... args) throws JsonProcessingException {
-		LOGGER.debug("Debig active");
-		LOGGER.info("Started without parameters");
-
-
-		Config config = ConfigProvider.getConfig();
-
-		TreeMap<String, String> stringStringTreeMap = new TreeMap<>(StreamSupport.stream(config.getPropertyNames()
-						.spliterator(), false)
-				.collect(Collectors.toMap(p -> p, p -> config.getConfigValue(p).getRawValue())));
-
-		LOGGER.info("Config: {}", (new ObjectMapper()).writeValueAsString(stringStringTreeMap));
-
-		//		return new TreeMap<>(StreamSupport.stream(config.getPropertyNames().spliterator(), false)
+//package org.acme;
+//
+//import java.util.Iterator;
+//import java.util.Set;
+//import java.util.TreeMap;
+//import java.util.stream.Collectors;
+//import java.util.stream.StreamSupport;
+//
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import io.quarkus.runtime.QuarkusApplication;
+//import io.quarkus.runtime.annotations.QuarkusMain;
+//import javax.enterprise.inject.spi.BeanManager;
+//import javax.inject.Inject;
+//import org.acme.rest.client.Extension;
+//import org.acme.rest.client.ExtensionsService;
+//import org.eclipse.microprofile.config.Config;
+//import org.eclipse.microprofile.config.ConfigProvider;
+//import org.eclipse.microprofile.rest.client.inject.RestClient;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//@QuarkusMain
+//public class ApplicationMain implements QuarkusApplication {
+//	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationMain.class);
+//
+//	@RestClient
+//	ExtensionsService extensionsService;
+//
+//
+//	@Override
+//	public int run(String... args) throws JsonProcessingException {
+//		LOGGER.debug("Debig active");
+//		LOGGER.info("Started without parameters");
+//
+//
+//		Config config = ConfigProvider.getConfig();
+//
+//		TreeMap<String, String> stringStringTreeMap = new TreeMap<>(StreamSupport.stream(config.getPropertyNames()
+//						.spliterator(), false)
 //				.collect(Collectors.toMap(p -> p, p -> config.getConfigValue(p).getRawValue())));
-
-
-		Set<Extension> byId = extensionsService.getById("io.quarkus:quarkus-rest-client-reactive");
-
-
-
-		Iterator<Extension> iterator = byId.iterator();
-		while (iterator.hasNext()) {
-			LOGGER.info("####");
-			LOGGER.info(iterator.next().name);
-
-		}
-
-		return 0;
-	}
-}
+//
+//		LOGGER.info("Config: {}", (new ObjectMapper()).writeValueAsString(stringStringTreeMap));
+//
+//		//		return new TreeMap<>(StreamSupport.stream(config.getPropertyNames().spliterator(), false)
+////				.collect(Collectors.toMap(p -> p, p -> config.getConfigValue(p).getRawValue())));
+//
+//
+//		Set<Extension> byId = extensionsService.getById("io.quarkus:quarkus-rest-client-reactive");
+//
+//
+//
+//		Iterator<Extension> iterator = byId.iterator();
+//		while (iterator.hasNext()) {
+//			LOGGER.info("####");
+//			LOGGER.info(iterator.next().name);
+//
+//		}
+//
+//		return 0;
+//	}
+//}
--- src/test/java/org/acme/ApplicationMainIT.java
+++ src/test/java/org/acme/ApplicationMainIT.java
@@ -1,18 +1,19 @@
 package org.acme;
 
-import io.quarkus.test.junit.main.LaunchResult;
+import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.main.QuarkusMainLauncher;
-import io.quarkus.test.junit.main.QuarkusMainTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.restassured.RestAssured.given;
 
-@QuarkusMainTest
+@QuarkusTest
 public class ApplicationMainIT {
 
 	@Test
-	public void testApplicationFlowWithoutDelay(QuarkusMainLauncher launcher) {
-		LaunchResult result = launcher.launch();
-		assertEquals(0, result.exitCode());
+	public void myTest() {
+		given()
+				.when().get("/hello")
+				.then()
+				.statusCode(200);
 	}
 }
